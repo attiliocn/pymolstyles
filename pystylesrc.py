913662da54bc97303a7fc617912120222172437a
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-01-20 11:06:44 -0300

    fix: system independent paths for rc file

--- pystylesrc.py
+++ pystylesrc.py
@@ -1,12 +1,13 @@
 # pymolstyles_dir should be defined in .pymolrc file
 
+import os
+
 if pymolstyles_dir:
     #External Scripts
-    cmd.run(f"{pymolstyles_dir}/external/cgo_arrow.py")
-    
+    cmd.run(os.path.join(f"{pymolstyles_dir}", 'external', 'cgo_arrow.py'))
     # PyMolStyles Scripts
-    cmd.run(f"{pymolstyles_dir}/legacy/default.py")
-    cmd.run(f"{pymolstyles_dir}/legacy/plot_sterimol.py")
-    cmd.run(f"{pymolstyles_dir}/legacy/buried_volume.py")
+    cmd.run(os.path.join(f"{pymolstyles_dir}", 'legacy', 'default.py'))
+    cmd.run(os.path.join(f"{pymolstyles_dir}", 'legacy', 'plot_sterimol.py'))
+    cmd.run(os.path.join(f"{pymolstyles_dir}", 'legacy', 'buried_volume.py'))
 else:
     raise
