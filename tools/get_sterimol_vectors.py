cb5e3da2408d7dbc6a7de8eda7637d550ec8bee4
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2023-06-03 12:03:05 -0300

    feat: calculate compound features from sterimol values

--- tools/get_sterimol_vectors.py
+++ tools/get_sterimol_vectors.py
@@ -10,6 +10,15 @@ parser.add_argument('atom1', type=int)
 parser.add_argument('atom2', type=int, nargs='+')
 args = parser.parse_args()
 
+
+def get_angle(v1, v2):
+    inner = np.inner(v1, v2)
+    norms = np.linalg.norm(v1) * np.linalg.norm(v2)
+    cos = inner / norms
+    rad = np.arccos(np.clip(cos, -1.0, 1.0))
+    deg = np.rad2deg(rad)
+    return deg
+
 elements, coordinates = morfeus.read_xyz(args.file)
 
 sterimol = morfeus.Sterimol(elements, coordinates, args.atom1, args.atom2)
@@ -20,6 +29,13 @@ print(f'Sterimol results for {args.file}')
 print("{:<20}{:<20.5}".format("L (uncorrected)", sterimol.L_value_uncorrected))
 print("{:<20}{:<20.5}".format("B1", sterimol.B_1_value))
 print("{:<20}{:<20.5}".format("B5", sterimol.B_5_value))
+print()
+print("{:<20}{:<20.1f}".format("Angle <L,B1>", get_angle(sterimol.L, sterimol.B_1)))
+print("{:<20}{:<20.1f}".format("Angle <L,B5>", get_angle(sterimol.L, sterimol.B_5)))
+print("{:<20}{:<20.1f}".format("Angle <B1,B5>", get_angle(sterimol.B_1, sterimol.B_5)))
+print("{:<20}{:<20.5}".format("B1/L", sterimol.B_1_value/sterimol.L_value_uncorrected))
+print("{:<20}{:<20.5}".format("B5/L", sterimol.B_5_value/sterimol.L_value_uncorrected))
+print("{:<20}{:<20.5}".format("B1/B5", sterimol.B_1_value/sterimol.B_5_value))
 print()
 
 sterimol_vectors = np.zeros(9).reshape(3,3)
